b6d3981217deec5b70eb778d4a7f2a559438ceb6
Alfonso Miguel Hernández <99462404+FonzHdz@users.noreply.github.com>
2025-03-25 19:25:06 -0500

    JWT Inactivo

--- src/main/java/com/harmoniChat/app_hc/api/v1/controllers/auth/RegisterRequest.java
+++ src/main/java/com/harmoniChat/app_hc/api/v1/controllers/auth/RegisterRequest.java
@@ -1,15 +1,15 @@
-package com.harmoniChat.app_hc.api.v1.controllers.auth;
-
-public record RegisterRequest (
-        String firstName,
-        String lastName,
-        String documentType,
-        String documentNumber,
-        String phoneNumber,
-        String email,
-        String password,
-        String gender,
-        String role
-){
-
-}
+//package com.harmoniChat.app_hc.api.v1.controllers.auth;
+//
+//public record RegisterRequest (
+//        String firstName,
+//        String lastName,
+//        String documentType,
+//        String documentNumber,
+//        String phoneNumber,
+//        String email,
+//        String password,
+//        String gender,
+//        String role
+//){
+//
+//}
